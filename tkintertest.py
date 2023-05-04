1c630853ea232ea42aab7fec8e03ad7f4f5387b5
[Bliss 0910] <[jw990910@gmail.com]>
2023-05-05 01:50:13 +0900

    Added Undo and Apply Filter function, UI changes & canvasImage changes

--- tkintertest.py
+++ tkintertest.py
@@ -11,12 +11,14 @@ class canvasImage:
     def __init__(self):
         self.image = None   #Image to be manipulated (PIL.Image)
         self.image_tk = None #Image to be displayed (PIL.ImageTk.PhotoImage)
+        self.preview_image = None #Image for previewing filter
 
 def add_image():
     path = filedialog.askopenfilename(
         initialdir=os.getcwd(),
     )
     canvasImage.image = Image.open(path)
+    canvasImage.preview_image = Image.open(path)
     canvasImage.image_tk = ImageTk.PhotoImage(canvasImage.image)
     
     main_canvas.config(width=10, height=10)
@@ -25,14 +27,14 @@ def add_image():
 
 def add_filter():
     """Challenge1: Due to incompatible variable format of Wand.image and ImageTk.PhotoImage, 
-    it is necessary to convert and pass data in bytes format to each package"""
+    it is necessary to convert and pass data in bytes format when converting between package"""
     #Convert PIL image to byteIO (blob/bytes)
     buf = io.BytesIO()
     canvasImage.image.save(buf, format="PNG")
     contents = buf.getvalue()
 
     with wand_img(blob=contents) as img:
-        img.blur(radius=0, sigma=3)
+        img.blur(radius=3, sigma=3)
         img.format = 'png'
 
         #Convert wand image to blob(bytes) for ImageTk/Image to read
@@ -54,7 +56,10 @@ def save_image():
     if path:
         os.chdir(os.path.dirname(path.name))
         canvasImage.image.save(path.name)
-        
+
+var = tk.DoubleVar()
+def get_value():
+    print(var.get())
 
 """ window.columnconfigure(0, weight=1)
 window.rowconfigure(0, weight=1) """
@@ -90,7 +95,13 @@ side_frame_addfilter.grid(row=2, column=1, pady=10, padx=10)
 bot_left_frame = tk.Frame(master=window, height=250, width=200, relief="ridge", borderwidth=2)
 bot_left_frame.grid(row=2,column=0, rowspan=2, columnspan=4, sticky="NSW", padx=1, pady=1)
 
+
 bot_right_frame = tk.Frame(master=window, height=250, width=200, relief="ridge", borderwidth=2)
 bot_right_frame.grid(row=2, column=9, rowspan=2, columnspan=4, sticky="NES", padx=1, pady=1)
+bot_right_frame.grid_propagate(0)
+slider = tk.Scale(master=bot_right_frame, from_=0.0, to=5.0, resolution=0.1, orient="horizontal", width=10, length=150, label="Slider", variable=var)
+slider.set(2.5)
+slider.grid(row=0, column=0, padx=15)
+
 
 window.mainloop()
