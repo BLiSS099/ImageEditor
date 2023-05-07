6835e8964f47292060f1ef199830e2abd22c2285
[Bliss 0910] <[jw990910@gmail.com]>
2023-05-08 03:20:01 +0900

    Added new image effects functions

--- tkintertest.py
+++ tkintertest.py
@@ -38,7 +38,43 @@ def add_image():
         ))
     main_canvas.create_image(canvas_width/2, canvas_height/2, anchor="center", image=main_canvas.image)
 
-def add_filter():
+def noise():
+    buf = io.BytesIO()
+    canvasImage.image.save(buf, format="PNG")
+    contents = buf.getvalue()
+
+    with wand_img(blob=contents) as img:
+        img.noise("gaussian", attenuate=1.0)
+        img.format = 'png'
+
+        blob = img.make_blob()
+        canvasImage.preview_image = Image.open(io.BytesIO(blob)) 
+        main_canvas.image = ImageTk.PhotoImage(canvasImage.preview_image.resize(
+            (int(canvasImage.preview_image.width/1.5), int(canvasImage.preview_image.height/1.5)), 
+            resample=Image.LANCZOS
+            ))
+        main_canvas.create_image(canvas_width/2, canvas_height/2, anchor="center", image=main_canvas.image)
+    apply_filter_btn["state"] = "normal"
+
+def charcoal():
+    buf = io.BytesIO()
+    canvasImage.image.save(buf, format="PNG")
+    contents = buf.getvalue()
+
+    with wand_img(blob=contents) as img:
+        img.charcoal(radius=1.5, sigma=0.5)
+        img.format = 'png'
+
+        blob = img.make_blob()
+        canvasImage.preview_image = Image.open(io.BytesIO(blob)) 
+        main_canvas.image = ImageTk.PhotoImage(canvasImage.preview_image.resize(
+            (int(canvasImage.preview_image.width/1.5), int(canvasImage.preview_image.height/1.5)), 
+            resample=Image.LANCZOS
+            ))
+        main_canvas.create_image(canvas_width/2, canvas_height/2, anchor="center", image=main_canvas.image)
+    apply_filter_btn["state"] = "normal"
+
+def blur():
     """Challenge1: Due to incompatible variable format of Wand.image and ImageTk.PhotoImage, 
     it is necessary to convert and pass data in bytes format when converting between package"""
 
@@ -96,33 +132,37 @@ window.rowconfigure([0,2], weight=1)
 main_canvas = tk.Canvas(master=window, height=250, width=850, relief="ridge", borderwidth=2)
 main_canvas.grid(row=0, column=5, rowspan=4, columnspan=4, sticky="NESW") 
 
-top_left_frame = tk.Frame(master=window, height=250, width=230, relief="ridge", borderwidth=2)
+top_left_frame = tk.Frame(master=window, height=250, width=250, relief="ridge", borderwidth=2)
 top_left_frame.grid(row=0, column=0, rowspan=2, columnspan=4, sticky="NSW", padx=1, pady=1) 
 top_left_frame.grid_propagate(0)
-add_image_btn = tk.Button(top_left_frame, text="add image", relief="raised", padx=10, command=add_image)
+add_image_btn = tk.Button(top_left_frame, text="Add image", relief="raised", padx=10, command=add_image)
 add_image_btn.grid(row=0, column=0, padx=10, pady=15)
-save_image_btn = tk.Button(top_left_frame, text="save image", relief="raised", padx=10, command=save_image)
+save_image_btn = tk.Button(top_left_frame, text="Save image", relief="raised", padx=10, command=save_image)
 save_image_btn.grid(row=0, column=1, padx=10, pady=15)
 
-top_right_frame = tk.Frame(master=window, height=250, width=230, relief="ridge", borderwidth=2)
+top_right_frame = tk.Frame(master=window, height=250, width=250, relief="ridge", borderwidth=2)
 top_right_frame.grid(row=0, column=9, rowspan=2, columnspan=4, sticky="NES", padx=1, pady=2)
 top_right_frame.grid_propagate(0)
-side_frame_addfilter = tk.Button(top_right_frame, text="Add filter", relief="raised", padx=10, command=add_filter)
-side_frame_addfilter.grid(row=2, column=1, pady=10, padx=10)
+side_frame_addfilter = tk.Button(top_right_frame, text="Blur", relief="raised", padx=10, command=blur)
+side_frame_addfilter.grid(row=2, column=1, padx=5, pady=10)
+noise_btn = tk.Button(top_right_frame, text="Noise", relief="raised", padx=10, command=noise)
+noise_btn.grid(row=2, column=2, padx=5, pady=10)
+charcoal_btn = tk.Button(top_right_frame, text="Charcoal", relief="raised", padx=10, command=charcoal)
+charcoal_btn.grid(row=2, column=3, padx=5, pady=10)
 
-bot_left_frame = tk.Frame(master=window, height=250, width=230, relief="ridge", borderwidth=2)
+bot_left_frame = tk.Frame(master=window, height=250, width=250, relief="ridge", borderwidth=2)
 bot_left_frame.grid(row=2,column=0, rowspan=2, columnspan=4, sticky="NSW", padx=1, pady=1)
 
-bot_right_frame = tk.Frame(master=window, height=250, width=230, relief="ridge", borderwidth=2)
+bot_right_frame = tk.Frame(master=window, height=250, width=250, relief="ridge", borderwidth=2)
 bot_right_frame.grid(row=2, column=9, rowspan=2, columnspan=4, sticky="NES", padx=1, pady=1)
 bot_right_frame.grid_propagate(0)
-slider = tk.Scale(master=bot_right_frame, from_=1.0, to_=10.0, resolution=0.1, orient="horizontal", width=10, length=150, label="Slider", variable=var)
-slider.set(4.0)
+slider = tk.Scale(master=bot_right_frame, from_=1.0, to_=20.0, resolution=0.1, orient="horizontal", width=10, length=150, label="Slider", variable=var)
+slider.set(6.0)
 slider.grid(row=0, column=0, padx=15)
 apply_filter_btn = tk.Button(master=bot_right_frame, text="Apply filter", relief="raised", command=apply_filter, state="disabled")
 apply_filter_btn.grid(row=1, column=0)
 undo_btn = tk.Button(master=bot_right_frame, text="Undo", relief="raised", command=undo_filter)
-undo_btn.grid(row=2, column=0)
+undo_btn.grid(row=1, column=1)
 
 main_canvas.bind("<Configure>", resizing)
 
