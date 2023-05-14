64ee55f167f3e54015226b459969023fd940c676
[Bliss 0910] <[jw990910@gmail.com]>
2023-05-15 01:53:58 +0900

    Added new image effects

--- tkintertest.py
+++ tkintertest.py
@@ -17,6 +17,7 @@ class canvasImage:
         self.image = None   #Image to be manipulated (PIL.Image)
         self.preview_image = None #Image for previewing filter 
 
+#Allow resizing of image on main canvas
 def resizing(e):
     global canvas_width, canvas_height
     window.update() #Tkinter root needs to be updated for getting width and height value of main_canvas
@@ -31,13 +32,13 @@ def render_image(img):
             ))
     main_canvas.create_image(canvas_width/2, canvas_height/2, anchor="center", image=main_canvas.image)
 
+#Download image from URL
 def download():
     url = simpledialog.askstring("URL", "Paste URL here")
     try:
         img = requests.get(url)
         canvasImage.image = Image.open(io.BytesIO(img.content))
         canvasImage.preview_image = Image.open(io.BytesIO(img.content))
-        
         render_image(canvasImage.preview_image)
 
     except UnidentifiedImageError:
@@ -84,17 +85,25 @@ def edge(img):
 def blueshift(img):
     img.blue_shift(factor=options_factor.get())
 
+def kuwahara(img):
+    img.kuwahara(radius=options_radius.get(), sigma=options_sigma.get())
+
+def sepia(img):
+    img.sepia_tone(threshold=options_threshold.get())
+
 #Filter list to be displayed in widgets
-filter_list = {
+effects_list = {
     'Blur': blur,
     'Noise': noise,
     'Charcoal': charcoal,
     'Edge': edge,
-    'Blue Shift': blueshift
+    'Blue Shift': blueshift,
+    'Kuwahara': kuwahara,
+    'Sepia Tone': sepia
 } 
 
 def add_effects(image_effect): #Passing image effects functions as parameters
-    """Challenge1: Due to incompatible variable format of Wand.image and ImageTk.PhotoImage, 
+    """Due to incompatible variable format of Wand.image and ImageTk.PhotoImage, 
     it is necessary to convert and pass data in bytes format when converting between package"""
     
     #Convert PIL image to byteIO (blob/bytes)
@@ -109,7 +118,7 @@ def add_effects(image_effect): #Passing image effects functions as parameters
         blob = img.make_blob()
         canvasImage.preview_image = Image.open(io.BytesIO(blob)) 
         render_image(canvasImage.preview_image)
-    apply_filter_btn["state"] = "normal"
+    apply_effect_btn["state"] = "normal"
 
 def apply_effects():
     #Applying effects to original image (canvasImage.image)
@@ -119,7 +128,7 @@ def apply_effects():
 def undo_effects():
     #Revert filter and set main_canvas.image to original (canvasImage.image)
     render_image(canvasImage.image)
-    apply_filter_btn["state"] = "disabled"
+    apply_effect_btn["state"] = "disabled"
     print("Filter undo-ed!")
 
 def save_image():
@@ -140,8 +149,8 @@ options_radius = tk.DoubleVar() #Radius value (slider widget)
 options_attenuate = tk.DoubleVar() #Attenuate value (slider widget)
 options_sigma = tk.DoubleVar() #Sigma value (slider widget)
 options_factor = tk.DoubleVar() #Factor vlaue (slider widget)
+options_threshold = tk.DoubleVar() #Threshold value (slider widget)
 options_type = tk.StringVar() #For accessing selected effect type
-
 
 def blur_options():
     title = tk.Label(bot_right_frame, text="Blur Options")
@@ -218,13 +227,39 @@ def edge_options():
     title.grid(row=0, column=0,padx=5, pady=5, sticky="w")
     radius.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
     return(title, radius)
-    
+
+def kuwahara_options():
+    title = tk.Label(bot_right_frame, text="Kuwahara effect options")
+    radius = tk.Scale(bot_right_frame, from_=1.0, to_=8.0, resolution=0.01, orient="horizontal", 
+                      width=10, length=200,label="Radius", variable=options_radius, relief="ridge", borderwidth=2)
+    radius.set(2.0)
+    sigma = tk.Scale(bot_right_frame, from_=0.0, to_=5.0, resolution=0.1, orient="horizontal", 
+                      width=10, length=200,label="Sigma", variable=options_sigma, relief="ridge", borderwidth=2)
+    sigma.set(2.0)
+
+    title.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+    radius.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
+    sigma.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+    return(title, radius, sigma)
+
+def sepia_options():
+    title = tk.Label(bot_right_frame, text="Sepia Tone effect options")
+    threshold = tk.Scale(bot_right_frame, from_=0.1, to_=1.0, resolution=0.01, orient="horizontal", 
+                      width=10, length=200,label="Threshold", variable=options_threshold, relief="ridge", borderwidth=2)
+    threshold.set(0.5)
+
+    title.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+    threshold.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
+    return(title, threshold)
+
 options_widgets = {
     "Blur": blur_options,
     "Noise": noise_options,
     "Charcoal": charcoal_options,
     "Edge": edge_options,
-    "Blue Shift": blueshift_options
+    "Blue Shift": blueshift_options,
+    "Kuwahara": kuwahara_options,
+    "Sepia Tone": sepia_options
 }
 
 def render_widget(key):
@@ -235,6 +270,7 @@ def render_widget(key):
     print(f"Rendering: {selected_filter}")
     selected_filter() #Run the function and render widgets for selected options
 
+""" Tkinter GUI code """
 main_canvas = tk.Canvas(master=window, height=250, width=850, relief="ridge", borderwidth=2)
 main_canvas.grid(row=0, column=5, rowspan=4, columnspan=4, sticky="NESW") 
 
@@ -249,9 +285,9 @@ url_submit_label = tk.Label(top_left_frame, text="URL Download : ")
 url_submit_btn = tk.Button(top_left_frame, text="Download", relief="raised", padx=10, command=download)
 save_image_btn = tk.Button(top_left_frame, text="Save image", relief="raised", padx=10, command=save_image)
 
-url_submit_label.grid(row=0, column=0, padx=20, pady=5)
+url_submit_label.grid(row=0, column=0, padx=20, pady=10)
+url_submit_btn.grid(row=0, column=1, padx=20, pady=10)
 or_label.grid(row=1, column=0, columnspan=2, pady=5)
-url_submit_btn.grid(row=0, column=1, padx=20, pady=5)
 add_image_label.grid(row=2, column=0, padx=5, pady=5)
 add_image_btn.grid(row=2, column=1, padx=10, pady=5)
 save_image_btn.grid(row=3, column=1, padx=10, pady=20)
@@ -261,25 +297,23 @@ top_right_frame.grid(row=0, column=9, rowspan=2, columnspan=4, sticky="NES", pad
 top_right_frame.grid_propagate(0)
 top_right_frame.rowconfigure(1, weight=1) #Allows second row of the frame to stick to bottom
 
-filters_var = tk.StringVar()
-
-filters = ttk.OptionMenu(
+effects_var = tk.StringVar()
+image_effects = ttk.OptionMenu(
     top_right_frame,
-    filters_var,
-    "Select a filter",
-    *filter_list.keys(),
+    effects_var,
+    "Select an effect",
+    *effects_list.keys(),
     command=render_widget
 )
-
-filter_label = tk.Label(top_right_frame, text="Filter effects :")
-preview_filter_btn = tk.Button(top_right_frame, text="Preview Filter", relief="raised", command=lambda: add_effects(filter_list[filters_var.get()]))
-apply_filter_btn = tk.Button(top_right_frame, text="Apply filter", relief="raised", command=apply_effects, state="disabled")
+effects_label = tk.Label(top_right_frame, text="Image effects :")
+preview_effect_btn = tk.Button(top_right_frame, text="Preview effect", relief="raised", command=lambda: add_effects(effects_list[effects_var.get()]))
+apply_effect_btn = tk.Button(top_right_frame, text="Apply effect", relief="raised", command=apply_effects, state="disabled")
 undo_btn = tk.Button(top_right_frame, text="Undo", relief="raised", command=undo_effects)
 
-filter_label.grid(row=0, column=0, padx=5, pady=5)
-filters.grid(row=0, column=1, columnspan=3, padx=5, pady=5)
-preview_filter_btn.grid(row=1, column=0, padx=5, pady=5, sticky="ws")
-apply_filter_btn.grid(row=1, column=1, padx=5, pady=5, sticky="s")
+effects_label.grid(row=0, column=0, padx=5, pady=10)
+image_effects.grid(row=0, column=1, columnspan=3, padx=5, pady=10)
+preview_effect_btn.grid(row=1, column=0, padx=5, pady=5, sticky="ws")
+apply_effect_btn.grid(row=1, column=1, padx=5, pady=5, sticky="s")
 undo_btn.grid(row=1, column=2, padx=5, pady=5, sticky="es")
 
 bot_left_frame = tk.Frame(master=window, height=250, width=250, relief="ridge", borderwidth=2)
